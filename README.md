eca34aeca40b5feaa4c26bba7f8dce13b237b4d2
Valeria <valeriatlou0913@gmail.com>
2026-05-04 01:33:37 -0400

    Se agrega microservicio

---
+++ README.md
@@ -0,0 +1,2 @@
+# Autores-BibliotecaProyecto
+Microservicio que contiene catálogo de autores del proyecto de biblioteca para asignatura Desarrollo Fullstack I.
---
+++ src/main/java/cl/bibliotecaproyecto/autores/repository/AutorRepository.java
@@ -0,0 +1,7 @@
+package cl.bibliotecaproyecto.autores.repository;
+
+import cl.bibliotecaproyecto.autores.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
